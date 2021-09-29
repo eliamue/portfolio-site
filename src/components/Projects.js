f2e466f9f7d41d2c6721b6ebf03b53289100b246
Elise Muellerleile <eliamue@live.com>
2021-09-29 13:40:14 -0500

    styling moar

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import '../styles/Projects.css';
-
-import ProjectList from './ProjectList';
-
+import safesrc from '../styles/safesrc.png';
 
 export default class Projects extends Component {
       render() {
@@ -11,8 +9,52 @@ export default class Projects extends Component {
                 <header>
                   <h1 className="proj-title">Projects</h1>
                 </header>
+
                 <section className="projects-container">
-                <ProjectList />
+                  <section className="safesrc">
+                    <img className="safesrc-logo" src={safesrc} alt="safeSRC logo" />
+                    <h1>
+                      <a
+                        className="safesrc-link"
+                        href="https://safesrc.netlify.app/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        safeSRC
+                      </a>
+                    </h1>
+                    <p>Description</p>
+                  </section>
+
+                  <section className="torr">
+                    <h1>
+                      <a
+                        className="torr-link"
+                        href="https://missaelortiz.github.io/The-Tales-of-Torr-The-Quest-to-Defeat-the-Everlasting-Dragon/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Tales of Torr: The Quest to Defeat the Everlasting
+                        Dragon
+                      </a>
+                    </h1>
+                    <p>Description</p>
+                  </section>
+
+                  <section className="triforce">
+                    <h1>
+                      <a
+                        className="triforce-link"
+                        href="https://triforce-trivia.netlify.app/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Triforce Trivia
+                      </a>
+                    </h1>
+                    <p>Description</p>
+                  </section>
+
                 </section>
               </div>
             );
